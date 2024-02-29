53d48725f68c14aaff6d14415b95c04f689d1bb3
Ahmedly SAM <ahmed.esam@duc.edu.iq>
2024-03-01 00:05:35 +0300

    assaignment completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
- 
+const divide = (a, b) => {
+    return a / b;
+  };
+  const square = (a) => {
+    return a * a;
+  };
+  const add = (a, b) => {
+    return a + b;
+  };
+  
+  //  1) has a function expression called divide
+  //     2) divide divides 2000 by 100
+  //     3) has an arrow function called square
+  //     4) square arrow function takes one parameter and multiplies it times itself
+  //     5) has an arrow function called add
+  //     6) add arrow function takes two parameters and adds them tog
+  
